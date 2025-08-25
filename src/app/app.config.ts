654e0ab9dee6282cfa32a31e00f1e7481280cf7a
dev-mohit-sachdev <devmohit.sachdev@gmail.com>
2025-08-26 00:51:49 +0530

    added the dotenv

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment.prod';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 

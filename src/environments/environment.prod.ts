9cf1d611cb4fda1b79d35ea7641075135d1c552c
dev-mohit-sachdev <devmohit.sachdev@gmail.com>
2025-08-26 00:40:22 +0530

    fixed the env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
-// src/environments/environment.prod.ts
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  },
+    apiKey: window['env'].FIREBASE_API_KEY,
+    authDomain: window['env'].FIREBASE_AUTH_DOMAIN,
+    projectId: window['env'].FIREBASE_PROJECT_ID,
+    storageBucket: window['env'].FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: window['env'].FIREBASE_MESSAGING_SENDER_ID,
+    appId: window['env'].FIREBASE_APP_ID
+  }
 };
